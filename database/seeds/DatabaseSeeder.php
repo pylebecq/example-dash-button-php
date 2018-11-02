21c9f08d05085e8f0e6c410d04f50a53f102d6a7
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2018-11-02 19:05:22 +0100

    Update database migration and seed files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // Create just one user
+        DB::table('users')->insert([
+            'email' => 'pierre-yves@zenaton.com',
+            'name' => 'Pierre-Yves',
+            'password' => Hash::make('secret'),
+            'created_at' => DB::raw('CURRENT_TIMESTAMP'),
+            'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
+        ]);
+
+        // Create just one product
+        DB::table('products')->insert([
+            'name' => 'Beer x24',
+            'price' => 1999,
+            'currency' => 'EUR',
+            'created_at' => DB::raw('CURRENT_TIMESTAMP'),
+            'updated_at' => DB::raw('CURRENT_TIMESTAMP'),
+        ]);
     }
 }
